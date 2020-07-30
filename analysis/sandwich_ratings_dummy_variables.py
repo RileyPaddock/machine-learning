c5b45f0d6feb772068a1abd069136ae401508d8f
RileyPaddock <rileyrpaddock@gmail.com>
2020-07-30 20:35:48 +0000

    Expanded more on sandwich formulas with interaction terms.

---
+++ analysis/sandwich_ratings_dummy_variables.py
@@ -0,0 +1,119 @@
+# Slices Beef | Tbsp Peanut Butter | Condiments  | Rating |
+# --------------------------------------------------------------
+#  0          | 0                  | -           |    1   |
+#  0          | 0                  | mayo        |    1   |
+#  0          | 0                  | jelly       |    4   |
+#  0          | 0                  | mayo, jelly |    0   |
+#  5          | 0                  | -           |    4   |
+#  5          | 0                  | mayo        |    8   |
+#  5          | 0                  | jelly       |    1   |
+#  5          | 0                  | mayo, jelly |    0   |
+#  0          | 5                  | -           |    5   |
+#  0          | 5                  | mayo        |    0   |
+#  0          | 5                  | jelly       |    9   |
+#  0          | 5                  | mayo, jelly |    0   |
+#  5          | 5                  | -           |    0   |
+#  5          | 5                  | mayo        |    0   |
+#  5          | 5                  | jelly       |    0   |
+#  5          | 5                  | mayo, jelly |    0   |
+
+import sys
+sys.path.append('src')
+from matrix import Matrix
+
+def apply_interactoin_terms(data,results):
+    matrix = data
+    data1 = []
+    data2 = []
+    for x in range(1,len(matrix.elements[0])+1):
+        for y in range(2,len(matrix.elements[0])+1):
+            if y > x:
+                data1.append(x-1)
+                data2.append(y-1)
+    for i in range(len(matrix.elements)):
+        for j in range(6):
+            matrix.elements[i].append(matrix.elements[i][data1[j]] * matrix.elements[i][data2[j]])
+
+    for i in range(len(matrix.elements)):
+        # matrix.elements[i].append(results.elements[i][0])
+        matrix.elements[i].insert(0,0)
+    
+    return matrix
+
+                    
+
+
+
+def regress(Inputs,Results):
+    x_tpose = Inputs.transpose()
+    data = ((x_tpose @ Inputs).inverse() @ (x_tpose @ Results)).elements
+    result = []
+    for elem in data:
+        result.append(elem[0])
+    return result
+
+def evaluate(data, coords):
+    result = data[0]
+    for i in range(len(coords)):
+        result += data[i+1]*coords[i]
+    i = 5
+    for x in range(len(coords)):
+        for y in range(1,len(coords)):
+            if y > x:
+                result += coords[x] * coords[y] * data[i]
+                i+=1
+    return result
+
+    
+        
+sandwich_data  = Matrix(elements = [[ 0, 0, 0, 0],
+ [0, 0, 1, 0],
+ [0, 0, 0, 1],
+ [0, 0, 1, 1],
+ [5, 0, 0, 0],
+ [5, 0, 1, 0],
+ [5, 0, 0, 1],
+ [5, 0, 1, 1],
+ [0, 5, 0, 0],
+ [0, 5, 1, 0],
+ [0, 5, 0, 1],
+ [0, 5, 1, 1],
+ [5, 5, 0, 0],
+ [5, 5, 1, 0],
+ [5, 5, 0, 1],
+ [5, 5, 1, 1]])
+sandwich_results = Matrix(elements = [[1], [1], [4], [0], [4], [8], [1], [0], [5], [0], [9], [0], [0], [0], [0], [0]])
+
+
+
+sandwich_data_with_interaction = apply_interactoin_terms(sandwich_data,sandwich_results)
+beta = regress(sandwich_data_with_interaction,sandwich_results)
+# 2 slices beef + mayo: __
+# 2 slices beef + jelly: __
+# 3 tbsp peanut butter + jelly: __
+# 3 tbsp peanut butter + jelly + mayo: ___
+# 2 slices beef + 3 tbsp peanut butter + jelly + mayo: ___
+
+
+print("\nCOEFFICIENTS")
+ 
+print("\n   bias term: "+str(beta[0]))
+ 
+print("\n   beef: "+str(beta[1]))
+print("\n   peanut butter: "+str(beta[2]))
+print("\n   mayo: "+str(beta[3]))
+print("\n   jelly: "+str(beta[4]))
+ 
+print("\n   beef & peanut butter: "+str(beta[5]))
+print("\n   beef & mayo: "+str(beta[6]))
+print("\n   beef & jelly: "+str(beta[7]))
+print("\n   peanut butter & mayo: "+str(beta[8]))
+print("\n   peanut butter & jelly: "+str(beta[9]))
+print("\n   mayo & jelly: "+str(beta[10]))
+
+print("\nRatings of certain sandwiches:")
+print("\n   2 slices beef + mayo: "+str(evaluate(beta,[2,0,1,0])))
+print("\n   2 slices beef + jelly: "+str(evaluate(beta,[2,0,0,1])))
+print("\n   3 tbsp peanut butter + jelly: "+str(evaluate(beta,[0,3,0,1])))
+print("\n   3 tbsp peanut butter + jelly + mayo: "+str(evaluate(beta,[0,3,1,1])))
+print("\n   2 slices beef + 3 tbsp peanut butter + jelly + mayo: "+str(evaluate(beta,[2,3,1,1])))
--- analysis/sandwich_ratings_interaction_terms.py
+++ analysis/sandwich_ratings_interaction_terms.py
@@ -2,14 +2,11 @@ import sys
 sys.path.append('src')
 from matrix import Matrix
 
-sandwich_data  = Matrix(elements = [[1,0,0],[1,1,0],[1,2,0],[1,4,0],[1,6,0],[1,0,2],[1,0,4],[1,0,6],[1,0,8],[1,2,2],[1,3,4]])
-sandwich_results = Matrix(elements = [[1],[2],[4],[8],[9],[2],[5],[7],[6],[0],[0]])
+
 
 def regress(Inputs,Results):
     x_tpose = Inputs.transpose()
     return ((x_tpose @ Inputs).inverse() @ (x_tpose @ Results)).elements
-
-betas = regress(sandwich_data,sandwich_results)
 
 # QUESTION 1
 # What is the model?
@@ -35,14 +32,6 @@ sandwich_results = Matrix(elements = [[1],[2],[4],[8],[9],[2],[5],[7],[6],[0],[0
 
 betas = regress(sandwich_data,sandwich_results)
 rating = str(betas[0][0]) +" + "+ str(betas[1][0])+" * (slices of beef) "+ str(betas[2][0])+" * (tbsp peanut butter)" + str(betas[3][0])+" * (slices of beef)(tbsp peanut butter)" 
-print("\n Formula for rating of sandwich:")
-print("\n   "+str(rating))
-def evaluate(coefficients,coords):
-    return coefficients[0]+coefficients[1]*coords[0]+coefficients[2]*coords[1] + coefficients[3]*(coords[0]*coords[1])
-print("\n Rating of Sandwich with 5 slices Roast Beef and 0 Tablespoons Peanut Butter:")
-print("\n   Rating: "+str(evaluate([betas[0][0],betas[1][0],betas[2][0],betas[3][0]],[5,0])))
-print("\n Rating of Sandwich with 5 slices Roast Beef and 5 Tablespoons Peanut Butter:")
-print("\n  Rating: "+str(evaluate([betas[0][0],betas[1][0],betas[2][0],betas[3][0]],[5,5])))
 
 # QUESTION 4
 # Fill out the table with the additional interaction term:

77b38fd999a2229ac86bcc17cc63df88a425646a
RileyPaddock <rileyrpaddock@gmail.com>
2020-08-17 05:04:03 +0000

    Added another logistic_regression along with a graph to show the estimation

--- analysis/logistic_regression.py
+++ analysis/logistic_regression.py
@@ -3,23 +3,20 @@ import sys
 sys.path.append('src')
 from matrix import Matrix
 
-def solve_coefficients(data):
-    X = Matrix([[1,x] for x,y in data])
-    Y = Matrix([[math.log((1/y) - 1)] for x,y in data])
-    x_tpose = X.transpose()
-    return ((x_tpose @ X).inverse() @ (x_tpose @ Y)).elements
+class Logistic_Regressor:
+    def __init__(self,data):
+        self.data = data
 
-data = [(10, 0.05), (100, 0.35), (1000, 0.95)]
+    def solve_coefficients(self):
+        X = Matrix([[1,x] for x,y in self.data])
+        Y = Matrix([[math.log((1/y) - 1)] for x,y in self.data])
+        x_tpose = X.transpose()
+        return ((x_tpose @ X).inverse() @ (x_tpose @ Y)).elements
 
-def evaluate(coefficients, time):
-    return 1/(1 + (math.exp(1)**(coefficients[0][0] + (coefficients[1][0]*time))))
+    def evaluate(self,coefficients, time):
+        return 1/(1 + (math.exp(1)**(coefficients[0][0] + (coefficients[1][0]*time))))
 
-def reverse_evaluate(coefficients,skill):
-    return (math.log((1/skill) - 1) - coefficients[0][0])/coefficients[1][0]
+    def reverse_evaluate(self,coefficients,skill):
+        return (math.log((1/skill) - 1) - coefficients[0][0])/coefficients[1][0]
 
-print("probability of beating a player with 500 hours:")
-decimal = evaluate(solve_coefficients(data),500)
-percentage = "{:.0%}".format(decimal)
-print("\n   "+str(percentage))
-print("skill level of average player: ")
-print("\n   "+str(reverse_evaluate(solve_coefficients(data),0.5)))
+

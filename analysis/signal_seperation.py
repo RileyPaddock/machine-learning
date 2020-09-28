cc88be6c0c015ab7e8bd444680d86910d69f71d4
RileyPaddock <rileyrpaddock@gmail.com>
2020-09-28 04:56:30 +0000

    Added signal_seperation test of linear_regressor

---
+++ analysis/signal_seperation.py
@@ -0,0 +1,87 @@
+import math
+import sys
+sys.path.append('src')
+from dataframe import DataFrame
+from linear_regressor import LinearRegressor
+def add_terms_a(data, degree):
+    adjusted_data = [[data[x][0]**i for i in range(degree+1)] + [data[x][1]] for x in range(len(data))]
+    data_dict = {}
+    for x in range(len(adjusted_data)):
+        for y in range(len(adjusted_data[0])):
+            if str(y) in data_dict:
+                data_dict[str(y)].append(adjusted_data[x][y])
+            else:
+                data_dict[str(y)] = [adjusted_data[x][y]]
+    return data_dict
+    
+def add_terms_b(data):
+    adjusted_data = [[math.sin(data[x][0]),math.cos(data[x][0]),math.sin(2*(data[x][0])),math.cos(2*(data[x][0]))] + [data[x][1]] for x in range(len(data))]
+    data_dict = {}
+    for x in range(len(adjusted_data)):
+        for y in range(len(adjusted_data[0])):
+            if str(y) in data_dict:
+                data_dict[str(y)].append(adjusted_data[x][y])
+            else:
+                data_dict[str(y)] = [adjusted_data[x][y]]
+    return data_dict
+data_a = [(0.0, 4.0),
+ (0.2, 8.9),
+ (0.4, 17.2),
+ (0.6, 28.3),
+ (0.8, 41.6),
+ (1.0, 56.5),
+ (1.2, 72.4),
+ (1.4, 88.7),
+ (1.6, 104.8),
+ (1.8, 120.1),
+ (2.0, 134.0),
+ (2.2, 145.9),
+ (2.4, 155.2),
+ (2.6, 161.3),
+ (2.8, 163.6),
+ (3.0, 161.5),
+ (3.2, 154.4),
+ (3.4, 141.7),
+ (3.6, 122.8),
+ (3.8, 97.1),
+ (4.0, 64.0),
+ (4.2, 22.9),
+ (4.4, -26.8),
+ (4.6, -85.7),
+ (4.8, -154.4)]
+
+data_dict_a = add_terms_a(data_a,3)
+df = DataFrame(data_dict_a)
+regression = LinearRegressor(df,"4")
+print(regression.coefficients)
+
+data_b = [(0.0, 7.0),
+ (0.2, 5.6),
+ (0.4, 3.56),
+ (0.6, 1.23),
+ (0.8, -1.03),
+ (1.0, -2.89),
+ (1.2, -4.06),
+ (1.4, -4.39),
+ (1.6, -3.88),
+ (1.8, -2.64),
+ (2.0, -0.92),
+ (2.2, 0.95),
+ (2.4, 2.63),
+ (2.6, 3.79),
+ (2.8, 4.22),
+ (3.0, 3.8),
+ (3.2, 2.56),
+ (3.4, 0.68),
+ (3.6, -1.58),
+ (3.8, -3.84),
+ (4.0, -5.76),
+ (4.2, -7.01),
+ (4.4, -7.38),
+ (4.6, -6.76),
+ (4.8, -5.22)]
+
+data_dict_b = add_terms_b(data_b)
+df = DataFrame(data_dict_b)
+regression = LinearRegressor(df,"4")
+print(regression.coefficients)
--- src/linear_regressor.py
+++ src/linear_regressor.py
@@ -1,4 +1,5 @@
 from matrix import Matrix
+from dataframe import DataFrame
 class LinearRegressor:
     def __init__(self,data_class, prediction_column):
         self.data = data_class
@@ -27,11 +28,28 @@ class LinearRegressor:
                     result[key1 + "_" + key2] = input_set[key1]*input_set[key2]
         result['constant'] = 1
         return result
+
     
     def predict(self, input_set):
         inputs = self.gather_all_inputs(input_set)
         result = sum([inputs[key]*self.coefficients[key] for key in inputs])
         return result
 
-
+# data_dict = {
+#     'beef': [0, 0, 0, 0, 5, 5, 5, 5, 0, 0, 0, 0, 5, 5, 5, 5],
+#     'pb': [0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5],
+#     'condiments': [[],['mayo'],['jelly'],['mayo','jelly'],
+#                    [],['mayo'],['jelly'],['mayo','jelly'],
+#                    [],['mayo'],['jelly'],['mayo','jelly'],
+#                    [],['mayo'],['jelly'],['mayo','jelly']],
+# }
+# df = DataFrame(data_dict)
+# df = df.create_dummy_variables()
+# df = df.append_pairwise_interactions()
+# df = df.append_columns({
+#     'constant': [1 for _ in range(len(data_dict['beef']))],
+#     'rating': [1, 1, 4, 0, 4, 8, 1, 0, 5, 0, 9, 0, 0, 0, 0, 0]
+# })
+# linear_regressor = LinearRegressor(df, prediction_column = 'rating')
+# print(linear_regressor.gather_all_inputs({ 'beef': 5, 'pb': 5, 'mayo': 1, 'jelly': 1}))
 

e2edf0d07800e405b8160fac5ec6b4019230d8ac
RileyPaddock <rileyrpaddock@gmail.com>
2021-01-29 05:37:54 +0000

    fixed random_tree

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -4,10 +4,13 @@ class DecisionTree:
     def __init__(self, split_metric, max_depth = 9999):
         self.split_metric = split_metric
         self.max_depth = max_depth
+        self.features = None
         self.root = None
 
     def fit(self, df):
+        df = df.append_columns({'node_index':[i for i in range(len(df.to_array()))]})
         self.root = Node(df, self.split_metric, 0)
+        self.features = [df.columns[i] for i in range(len(df.to_array()[0])-2)]
         while self.root.split(self.max_depth):
             pass
 
@@ -17,9 +20,9 @@ class DecisionTree:
         if level.impurity == 0:
             for key in level.class_counts:
                 return key 
-        elif point_list[ord(level.best_split[0])-120] >= level.best_split[1]:
+        elif point_list[self.features.index(level.node_split[0])] > level.node_split[1]:
             return self.classify(point,level.high)
-        elif point_list[ord(level.best_split[0])-120] < level.best_split[1]:
+        elif point_list[self.features.index(level.node_split[0])] <= level.node_split[1]:
             return self.classify(point,level.low)
 
 
@@ -28,6 +31,8 @@ class Node:
         self.df = df
         self.split_metric = split_metric
         self.depth = depth
+        self.num_vars = len(self.df.to_array()[0])-2
+        self.node_split = None
         self.low = None
         self.high = None
         self.row_indices = self.df.data_dict['node_index']
@@ -38,9 +43,9 @@ class Node:
             self.best_split = self.calc_best_split()
 
     def calc_class_counts(self):
-        class_count = {x[2]:0 for x in self.df.to_array()}
+        class_count = {x[len(x)-2]:0 for x in self.df.to_array()}
         for x in self.df.to_array():
-            class_count[x[2]]+=1
+            class_count[x[len(x)-2]]+=1
         return class_count
     
     def calc_impurity(self):
@@ -55,35 +60,43 @@ class Node:
         greater = []
         less = []
         for entry in self.df.to_array():
-            if entry[split[0]] >= split[1]:
+            if entry[split[0]] > split[1]:
                 greater.append(entry)
             else:
                 less.append(entry)
-        splits = [Node(DataFrame.from_array(greater,['x','y','class','node_index']),self.split_metric,self.depth, True),Node(DataFrame.from_array(less,['x','y','class','node_index']),self.split_metric,self.depth, True)]
+        splits = [Node(DataFrame.from_array(greater,self.df.columns),self.split_metric,self.depth, True),Node(DataFrame.from_array(less,self.df.columns),self.split_metric,self.depth, True)]
         for split in splits:
             to_be_summed.append((len(split.row_indices)/len(self.row_indices)) * split.impurity)
+        # 0.5 - 
         return self.impurity - sum(to_be_summed)
 
     def possible_splits(self):
         possible_splits = []
-        distinct = [[],[]]
-        for i in range(2):
+        features = [self.df.columns[i] for i in range(len(self.df.to_array()[0])-2)]
+        distinct = [[] for _ in range(self.num_vars)]
+        for i in range(self.num_vars):
             for entry in self.df.to_array():
                 if entry[i] not in distinct[i]:
                     distinct[i].append(entry[i])
-        for i in range(2):
+        for i in range(self.num_vars):
             for j in range(len(distinct[i])-1):
                 possible_splits.append((i,(distinct[i][j]+distinct[i][j+1])/2))
-        return DataFrame.from_array([[chr(120+entry[0]),entry[1],self.goodness_of_split(entry)] for entry in possible_splits],['axis','point','goodness of split'])
+        return DataFrame.from_array([[features[entry[0]],entry[1],self.goodness_of_split(entry)] for entry in possible_splits],['axis','point','goodness of split'])
     
     def calc_best_split(self, possible_splits = 'all'):
         if possible_splits == 'all':
             goodness = [x[2] for x in self.possible_splits.to_array()]
         else:
             goodness = [x[2] for x in possible_splits]
-        max_goodness_index = goodness.index(max(goodness))
-        return (self.possible_splits.to_array()[max_goodness_index][0],self.possible_splits.to_array()[max_goodness_index][1])
 
+        if len(goodness) == 0:
+            self.impurity = 0
+            majority = max([(self.class_counts[i],i) for i in self.class_counts])
+            self.class_counts = {majority[1]:majority[0]}
+        else:
+            max_goodness_index = goodness.index(max(goodness))
+
+            return (self.possible_splits.to_array()[max_goodness_index][0],self.possible_splits.to_array()[max_goodness_index][1])
     def split(self, max_depth):
         if self.depth < max_depth:
             if self.low is None and self.impurity != 0: 
@@ -92,6 +105,7 @@ class Node:
                     elif self.split_metric == 'random':
                         random_feature = random.choice(list(set([i for i,j,k in self.possible_splits.to_array()])))
                         split = self.calc_best_split(possible_splits = [entry for entry in self.possible_splits.to_array() if entry[0] == random_feature])
+                    self.node_split = split
                     self.low = Node(self.df.select_rows_where(
                         lambda x: x[split[0]] <= split[1]), self.split_metric, self.depth+1)
                     self.high = Node(self.df.select_rows_where(
--- tests/test_decision_tree.py
+++ tests/test_decision_tree.py
@@ -3,129 +3,67 @@ sys.path.append('src')
 from dataframe import DataFrame
 from decision_tree import DecisionTree
 
-data = [[2,13,'B'],[2,13,'B'],[2,13,'B'],[2,13,'B'],[2,13,'B'],[2,13,'B'],
-    [3,13,'B'],[3,13,'B'],[3,13,'B'],[3,13,'B'],[3,13,'B'],[3,13,'B'],
-    [2,12,'B'],[2,12,'B'],
-    [3,12,'A'],[3,12,'A'],
-    [3,11,'A'],[3,11,'A'],
-    [3,11.5,'A'],[3,11.5,'A'],
-    [4,11,'A'],[4,11,'A'],
-    [4,11.5,'A'],[4,11.5,'A'],
-    [2,10.5,'A'],[2,10.5,'A'],
-    [3,10.5,'B'],
-    [4,10.5,'A']]
 
-df = DataFrame.from_array(data, columns = ['x', 'y', 'class'])
-df = df.append_columns({'node_index':[i for i in range(len(df.to_array()))]})
-print("Testing splits using gini impurity")
-dt = DecisionTree('gini', 4)
-dt.fit(df)
-# print("\n Testing splits")
-# assert dt.root.best_split == ('y', 12.5)
-# assert dt.root.low.best_split == ('x', 2.5)
-# assert dt.root.low.low.best_split == ('y', 11.25)
-# assert dt.root.low.high.best_split == ('y', 11)
-# assert dt.root.low.high.low.best_split == ('x', 3.5)
-# print("     passed")
-
-# print("\n Testing classify")
-# assert dt.classify({'x': 2, 'y': 11.5}) == 'B'
-# assert dt.classify({'x': 2.5, 'y': 13}) == 'B'
-# assert dt.classify({'x': 4, 'y': 12}) == 'A'
-# assert dt.classify({'x': 3.25, 'y': 10.5}) == 'B'
-# assert dt.classify({'x': 3.75, 'y': 10.5}) == 'A'
-# print("     passed")
-
-data = [[i,'A'] for i in range(50)]+[[j,'B'] for j in range(50,100)]
-df = DataFrame.from_array(data, columns = ['x', 'class'])
-df = df.append_columns({'node_index':[i for i in range(len(df.to_array()))]})
+data = [[x,y,'positive' if x*y > 0 else 'negative'] for x in range(-5,6) if x != 0 for y in range(-5,6) if y != 0]
+df = DataFrame.from_array(data, columns = ['x','y', 'class'])
+print(len(df.to_array()))
 
 print("Testing random splits")
 dt = DecisionTree('random')
 dt.fit(df)
+total = 0
+correct = 0
+for entry in df.to_array():
+    total += 1
+    if dt.classify({'x':entry[0], 'y':entry[1]}) == entry[2]:
+        correct += 1
+print("Decision Tree:"+str((correct/total)))
 
-# df = DataFrame.from_array(
-#     [[1, 11, 'A'],
-#     [1, 12, 'A'],
-#     [2, 11, 'A'],
-#     [1, 13, 'B'],
-#     [2, 13, 'B'],
-#     [3, 13, 'B'],
-#     [3, 11, 'B']],
-#     columns = ['x', 'y', 'class']
-# )
-# dt = DecisionTree(df)
+data = [[x,y,'A'] for x in range(-5,6) if x!= 0 for y in range(-5,6) if y != 0] + [[x,y,'B'] for x in range(1,6) for y in range(1,6)] + [[x,y,'B'] for x in range(1,6) for y in range(1,6)]
+df = DataFrame.from_array(data, columns = ['x','y', 'class'])
+print(len(df.to_array()))
 
-# dt.root.row_indices
-# [0, 1, 2, 3, 4, 5, 6] # these are the indices of data points in the root node
 
-# dt.root.class_counts
-# {
-#     'A': 3,
-#     'B': 4
-# }
+print("Testing random splits")
+dt = DecisionTree('random')
+dt.fit(df)
+total = 0
+correct = 0
+for entry in df.to_array():
+    total += 1
+    if dt.classify({'x':entry[0], 'y':entry[1]}) == entry[2]:
+        correct += 1
+print("Decision Tree:"+str((correct/total)))
 
-# dt.root.impurity
-# 0.490 # rounded to 3 decimal places
+data = [[x,y,z,'positive' if x>0 and y>0 and z>0 else 'negative'] for x in range(-5,6) if x!=0 for y in range(-5,6) if y!=0 for z in range(-5,6) if z!=0]
+df = DataFrame.from_array(data, columns = ['x','y','z', 'class'])
+print(len(df.to_array()))
 
-# dt.root.possible_splits.to_array()
-# # dt.possible_splits is a dataframe with columns
-# # ['feature', 'value', 'goodness of split']
-# # Note: below is rounded to 3 decimal places
 
-# [['x', 1.5,  0.085],
-#  ['x', 2.5,  0.147],
-#  ['y', 11.5, 0.085],
-#  ['y', 12.5, 0.276]]
+print("Testing random splits")
+dt = DecisionTree('random')
+dt.fit(df)
+total = 0
+correct = 0
+for entry in df.to_array():
+    total += 1
+    if dt.classify({'x':entry[0], 'y':entry[1], 'z':entry[2]}) == entry[3]:
+        correct += 1
+print("Decision Tree:"+str((correct/total)))
 
-# dt.root.best_split
-# print("     passed")
+data = [[x,y,z,'A'] for x in range(-5,6) if x!= 0 for y in range(-5,6) if y != 0 for z in range(-5,6) if z != 0] + [[x,y,z,'B'] for x in range(1,6) for y in range(1,6) for z in range(1,6)] + [[x,y,z,'B'] for x in range(1,6) for y in range(1,6) for z in range(1,6)]
+df = DataFrame.from_array(data, columns = ['x','y','z', 'class'])
+print(len(df.to_array()))
 
-# print("\n Testing root best_split")
-# assert dt.root.best_split == ('y', 12.5)
-# print("     passed")
+print("Testing random splits")
+dt = DecisionTree('random')
+dt.fit(df)
+total = 0
+correct = 0
+for entry in df.to_array():
+    total += 1
+    if dt.classify({'x':entry[0], 'y':entry[1], 'z':entry[2]}) == entry[3]:
+        correct += 1
+print("Decision Tree:"+str((correct/total)))
 
-# print("\n Testing root low indices")
-# dt.split()
-# assert dt.root.low.row_indices == [0, 1, 2, 6]
-# print("     passed")
 
-# print("\n Testing root high indices")
-# assert dt.root.high.row_indices == [3, 4, 5]
-# print("     passed")
-
-# print("\n Testing root low impurity")
-# assert round(dt.root.low.impurity,3) == 0.375
-# print("     passed")
-
-# print("\n Testing root high impurity")
-# assert dt.root.high.impurity == 0
-# print("     passed")
-
-# print("\n Testing root low possible splits")
-# rounded = [[x[0],x[1],round(x[2],3)] for x in dt.root.low.possible_splits.to_array()]
-# assert rounded == [['x', 1.5,  0.125], ['x', 2.5,  0.375], ['y', 11.5, 0.042]]
-# print("     passed")
-
-# print("\n Testing root low best split")
-# assert dt.root.low.best_split == ('x', 2.5)
-# print("     passed")
-
-# print("\n Testing root low low indices")
-# dt.split()
-# assert dt.root.low.low.row_indices == [0, 1, 2]
-# print("     passed")
-
-# print("\n Testing root low high indices")
-# assert dt.root.low.high.row_indices == [6]
-# print("     passed")
-
-# dt = DecisionTree(df)
-
-# print("\n Testing fit")
-# dt.fit()
-
-# assert dt.root.high.row_indices == [3, 4, 5]
-# assert dt.root.low.low.row_indices == [0, 1, 2]
-# assert dt.root.low.high.row_indices == [6]
-# print("     passed")

d805bb831260262a28e14d1cc1fd85afe6dbef02
RileyPaddock <rileyrpaddock@gmail.com>
2021-03-08 22:06:53 +0000

    added parse line to titanic

--- analysis/titanic_analysis.py
+++ analysis/titanic_analysis.py
@@ -11,59 +11,95 @@ from logistic_regressor import LogisticRegressor
 from naive_bayes_classifier import NaiveBayesClassifier
 from k_nearest_neighbors_classifier import KNearestNeighborsClassifier
 
-path_to_datasets = '/home/runner/machine-learning/datasets/titanic/'
-filename = 'dataset_of_knowns.csv' 
-filepath = path_to_datasets + filename
+
+def parse_line(line,quote_symbol):
+    entries = []
+    current_entry = []
+    inside_quotes = False
+
+    for char in line+',':
+        if char == quote_symbol:
+            inside_quotes = not inside_quotes
+            current_entry.append(char)
+        elif char == "," and not inside_quotes:
+            entries.append(''.join(current_entry))
+            current_entry = []
+        else:
+            current_entry.append(char)
+
+    return entries
+
+line_1 = "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S"
+assert parse_line(line_1,"'") == ['1', '0', '3', "'Braund, Mr. Owen Harris'", 'male', '22', '1', '0', 'A/5 21171', '7.25', '', 'S']
+
+line_2 = '102,0,3,"Petroff, Mr. Pastcho (""Pentcho"")",male,,0,0,349215,7.8958,,S'
+assert parse_line(line_2,'"') == ['102', '0', '3', '"Petroff, Mr. Pastcho (""Pentcho"")"', 'male', '', '0', '0', '349215', '7.8958', '', 'S']
+
+line_3 = '187,1,3,"O\'Brien, Mrs. Thomas (Johanna ""Hannah"" Godfrey)",female,,1,0,370365,15.5,,Q'
+assert parse_line(line_3,'"') == ['187', '1', '3', '"O\'Brien, Mrs. Thomas (Johanna ""Hannah"" Godfrey)"', 'female', '', '1', '0', '370365', '15.5', '', 'Q']
+
+# path_to_datasets = '/home/runner/machine-learning/datasets/titanic/'
+# filename = 'dataset_of_knowns.csv' 
+# filepath = path_to_datasets + filename
 
 
-df = DataFrame.from_csv(filepath)
-df = df.remove_columns(["Name", "Ticket", "Fare", "Cabin"])
-pred = df.filter_columns(['Survived'])
-df = df.remove_columns(['Survived','PassengerId'])
-df = df.append_columns(pred.data_dict)
-df = df.apply('Sex', lambda s: 0 if s == 'male' else 1)
-df = df.apply('Embarked', lambda s: 0 if s =='S' else 1 if s == 'C' else 2)
-df = df.apply('Age', lambda i: i if isinstance(i, float) else 0)
+# df = DataFrame.from_csv(filepath)
+# df = df.remove_columns(["Name", "Ticket", "Fare", "Cabin"])
+# pred = df.filter_columns(['Survived'])
+# df = df.remove_columns(['Survived','PassengerId'])
+# df = df.append_columns(pred.data_dict)
+# df = df.apply('Sex', lambda s: 0 if s == 'male' else 1)
+# df = df.apply('Embarked', lambda s: 0 if s =='S' else 1 if s == 'C' else 2)
+# df = df.apply('Age', lambda i: i if isinstance(i, float) else 0)
 
-print(df.columns)
+# print(df.columns)
 
-path_to_datasets = '/home/runner/machine-learning/datasets/titanic/'
-filename = 'unkowns_to_predict.csv' 
-filepath = path_to_datasets + filename
+# path_to_datasets = '/home/runner/machine-learning/datasets/titanic/'
+# filename = 'unkowns_to_predict.csv' 
+# filepath = path_to_datasets + filename
 
-df2 = DataFrame.from_csv(filepath)
-df2 = df2.remove_columns(["Name", "Ticket", "Fare", "Cabin", "PassengerId"])
-df2 = df2.apply('Sex', lambda s: 0 if s == 'male' else 1)
-df2 = df2.apply('Embarked', lambda s: 0 if s =='S' else 1 if s == 'C' else 2)
-df2= df2.apply('Age', lambda i: i if isinstance(i, float) else 0)
+# df2 = DataFrame.from_csv(filepath)
+# df2 = df2.remove_columns(["Name", "Ticket", "Fare", "Cabin", "PassengerId"])
+# df2 = df2.apply('Sex', lambda s: 0 if s == 'male' else 1)
+# df2 = df2.apply('Embarked', lambda s: 0 if s =='S' else 1 if s == 'C' else 2)
+# df2= df2.apply('Age', lambda i: i if isinstance(i, float) else 0)
 
-result = [['PassengerId', 'Survived']]
+# result = [['PassengerId', 'Survived']]
 
-# liR = LinearRegressor(df, 'Survived')
-# print(liR.coefficients)
+# # liR = LinearRegressor(df, 'Survived')
+# # print(liR.coefficients)
+# # i = 0
+# # for test in df2.to_array():
+# #     pred = liR.predict({df2.columns[i]:test[i] for i in range(len(test)-1)})
+# #     output = 1 if pred >= 0.5 else 0
+# #     result.append([i+892, output])
+# #     i+=1
+
+# print("start")
+
 # i = 0
+# dt = RandomForest(1000,10,1)
+# dt.fit(df)
 # for test in df2.to_array():
-#     pred = liR.predict({df2.columns[i]:test[i] for i in range(len(test)-1)})
-#     output = 1 if pred >= 0.5 else 0
-#     result.append([i+892, output])
-#     i+=1
+#     pred = dt.predict({df2.columns[i]:test[i] for i in range(len(test))})
+#     result.append([i+892, int(pred)])
+#     i += 1
 
-print("start")
+# spamWriter = csv.writer(open('predictions.csv', 'w'), delimiter=',',quotechar="'", quoting=csv.QUOTE_MINIMAL)
+# for row in result:
+#     #print(row)
+#     spamWriter.writerow(row)
 
-i = 0
-dt = RandomForest(1000,10,1)
-dt.fit(df)
-for test in df2.to_array():
-    pred = dt.predict({df2.columns[i]:test[i] for i in range(len(test))})
-    result.append([i+892, int(pred)])
-    i += 1
+# print('end')
 
-spamWriter = csv.writer(open('predictions.csv', 'w'), delimiter=',',quotechar="'", quoting=csv.QUOTE_MINIMAL)
-for row in result:
-    #print(row)
-    spamWriter.writerow(row)
 
-print('end')
+
+
+
+
+
+
+
 
 # sets = {'train':DataFrame.from_array([elem for elem in df.to_array() if df.to_array().index(elem)%2 == 0], df.columns), 'test': DataFrame.from_array([elem for elem in df.to_array() if df.to_array().index(elem)%2 == 1], df.columns)}
 # sets['train'] = sets['train'].remove_columns(['Age'])
